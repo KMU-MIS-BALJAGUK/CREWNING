9f25f6f1f1d0fa96ea61548f9a45c6c7f3e9f052
koobontak <kbt82883@gmail.com>
2025-11-02 01:37:40 +0900

    feat: 네비게이션바 초기화

--- lib/features/crew/presentation/crew_screen.dart
+++ lib/features/crew/presentation/crew_screen.dart
@@ -5,8 +5,6 @@ class CrewScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('크루 페이지가 준비 중입니다.'),
-    );
+    return const SizedBox.expand();
   }
 }
